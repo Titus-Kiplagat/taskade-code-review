c772edbefe9a6d98841bc69490475c2aae9b7a92
Titus Lagat <lagattyto@gmail.com>
2023-08-15 18:20:52 +0300

    Add tasks array that contains task objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+
+const tasks = [
+  {
+    index: 1,
+    description: 'Task 1',
+    Complete: true,
+  },
+  {
+    index: 2,
+    description: 'Task 2',
+    Complete: false,
+  },
+  {
+    index: 3,
+    description: 'Task 3',
+    Complete: false,
+  },
+];
