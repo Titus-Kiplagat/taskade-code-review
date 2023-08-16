d7d3e0ee5c2229b0ec752bf198c0746f5e247759
Titus Lagat <lagattyto@gmail.com>
2023-08-16 14:16:54 +0300

    Enhance task list rendering and deletion functionality

--- src/modules/renderList.js
+++ src/modules/renderList.js
@@ -1,9 +1,18 @@
 import createListMarkup from './createListMarkup.js';
-import tasks from './getTasksFromLocalStorage.js';
+import tasks from './getTasks.js';
+import removeTask from './removeTask.js';
 
 const renderList = () => {
   const todosListItems = document.getElementById('todos');
   todosListItems.innerHTML = tasks.map(createListMarkup).join('');
+
+  const deleteButtons = document.querySelectorAll('.remove-task');
+  deleteButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      removeTask(index);
+      renderList();
+    });
+  });
 };
 
 export default renderList;
