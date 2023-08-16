f8e7c57655279b5082c12857ca333427112b2da3
Titus Lagat <lagattyto@gmail.com>
2023-08-16 16:21:38 +0300

    Add content editable behaviour depending whether a task is complete

--- src/modules/createListMarkup.js
+++ src/modules/createListMarkup.js
@@ -3,7 +3,7 @@ const createListMarkup = (task) => `
       <div>
         <input type="checkbox" name="tasks" ${task.Complete ? 'checked' : ''
 } id="${task.index}" />
-        <span>${task.description}</span>
+        <span ${!task.complete ? 'contenteditable' : ''}>${task.description}</span>
       </div>
       <button title="Remove task" class="remove-task">
         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 128 512"><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
