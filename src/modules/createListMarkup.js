8769c5b9d32816264819a91115e6fd9c2cf2b5d7
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-08-21 22:30:48 +0300

    Enhance code readability and maintainability with function object destructuring

--- src/modules/createListMarkup.js
+++ src/modules/createListMarkup.js
@@ -1,9 +1,9 @@
-const createListMarkup = (task) => `
-    <li class="${task.complete ? 'completed' : ''}">
+const createListMarkup = ({ index, description, complete }) => `
+    <li class="${complete ? 'completed' : ''}">
       <div>
-        <input class="checkbox" type="checkbox" name="tasks" ${task.complete ? 'checked' : ''
-} id="${task.index}" />
-        <span ${!task.complete ? 'contenteditable' : ''}>${task.description}</span>
+        <input class="checkbox" type="checkbox" name="tasks" ${complete ? 'checked' : ''
+} id="${index}" />
+        <span ${!complete ? 'contenteditable' : ''}>${description}</span>
       </div>
       <button title="Remove task" class="remove-task">
         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 128 512"><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
